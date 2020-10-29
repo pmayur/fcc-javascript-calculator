6edcf1c65283d7bc7b71bf39407b9a514026d71b
pmayur <thisispatil@gmail.com>
2020-10-29 18:41:38 +0530

    Removes default generated files
    - Reformats all files (prettier)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>JS Calc</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
