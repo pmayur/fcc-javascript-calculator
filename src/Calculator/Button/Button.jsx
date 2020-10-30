3471114423dd31fc2c7847a2bd5c26fb0315abd1
pmayur <thisispatil@gmail.com>
2020-10-30 18:33:19 +0530

    Introduces Button component for Buttons

---
+++ src/Calculator/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Button.css";
+
+class Button extends React.Component {
+    render() {
+        return <div className="Button"></div>;
+    }
+}
+
+export default Button;
--- src/Calculator/CalcBody/CalcBody.jsx
+++ src/Calculator/CalcBody/CalcBody.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './CalcBody.css'
+import "./CalcBody.css";
+
+import Button from "../Button/Button";
 
 class CalcBody extends React.Component {
-
     render() {
         return (
             <div id="CalcBody">
+                <div id="num-pad"></div>
+                <div id="op-pad"></div>
             </div>
-        )
+        );
     }
 }
 
