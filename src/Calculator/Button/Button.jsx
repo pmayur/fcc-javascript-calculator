145083d80a570ad7307f2b8af7ae1f343e295928
pmayur <thisispatil@gmail.com>
2020-11-02 21:08:01 +0530

    Implements actions in Button Component

--- src/Calculator/Button/Button.jsx
+++ src/Calculator/Button/Button.jsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Button.css";
 
 import { useDispatch } from "react-redux";
-import { changeValue } from "../../actions";
-import { PADTYPE } from '../../utilities'
+import { changeValue, changeOperator, compute } from "../../actions";
+import { PADTYPE } from "../../utilities";
 
 function Button(props) {
     const dispatch = useDispatch();
+
+    const operate =
+        props.padtype === PADTYPE.NUMPAD
+            ? changeValue
+            : props.padtype === PADTYPE.OPPAD
+            ? changeOperator
+            : compute;
+
+    const keyPressHandler = (e) => {
+        if (e.key === props.buttonText) {
+            dispatch(operate(props.buttonText));
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("keypress", keyPressHandler);
+
+        return () => {
+            document.removeEventListner("keypress", keyPressHandler);
+        };
+    });
+
+    
 
     return (
         <div
             className="Button"
             id={props.id}
-            onClick={() => dispatch(changeValue(props.buttonText))}
+            onClick={() => {
+                dispatch(operate(props.buttonText));
+            }}
+            onKeyPress={keyPressHandler}
         >
             {props.buttonText}
         </div>
